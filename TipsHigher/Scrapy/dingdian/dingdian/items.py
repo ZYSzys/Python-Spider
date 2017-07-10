5d928adb564bcfe63fba4e409e74421c623e6578
ZYSzys <1220736035@qq.com>
2017-07-10 13:47:04 +0800

    add items

--- TipsHigher/Scrapy/dingdian/dingdian/items.py
+++ TipsHigher/Scrapy/dingdian/dingdian/items.py
@@ -24,3 +24,17 @@ class DingdianItem(scrapy.Item):
     category = scrapy.Field()
 
     name_id = scrapy.Field()
+
+class DcontentItem(scrapy.Item):
+	
+	id_name = scrapy.Field()
+
+	chaptercontent = scrapy.Field()
+
+	num = scrapy.Field()
+
+	chapterurl = scrapy.Field()
+
+	chaptername = scrapy.Field()
+
+	
