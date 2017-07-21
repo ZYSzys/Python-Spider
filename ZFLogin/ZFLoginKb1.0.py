c080c8b8f5e6f2a6fcdd339209f261e1b835349c
ZYSzys <1220736035@qq.com>
2017-07-21 14:00:40 +0800

    modify

--- ZFLogin/ZFLoginKb1.0.py
+++ ZFLogin/ZFLoginKb1.0.py
@@ -3,61 +3,111 @@
 
 __author__ = 'ZYSzys'
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-from bs4 import BeautifulSoup
+import requests
 import re
+import os
+import sys
 import urllib
+import urllib2
+from lxml import etree
+from PIL import Image
 
-#学号密码:
-usr_name = '2016........'
-pass_word = '........'
+class WHO:
+	def __init__(self, user, pswd):
+		self.user = user
+		self.pswd = pswd
 
-#浏览器驱动:
-driver = webdriver.Chrome()
-driver.get('http://115.236.84.162/default5.aspx')
+class Tool:
+	rmtb = re.compile('<br />|</br>|<br>')
+	rmtime1 = re.compile('<td align="Center" width="7%">.*?</td>')
+	rmtime2 = re.compile('<td class="noprint" align="Center".*?>.*?</td>')
+	def replace(self, x):
+		x = re.sub(self.rmtb, '---', x)
+		x = re.sub(self.rmtime1, '\n', x)
+		x = re.sub(self.rmtime2, '', x)
+		return x.strip()
 
-#寻找'class = login'输入帐号
-account = driver.find_element_by_name('TextBox1')
-account.clear()
-account.send_keys(usr_name)
+class ZAFU:
 
-#寻找'class = password'输入密码
-password = driver.find_element_by_name('TextBox2')
-password.clear()
-password.send_keys(pass_word)
+	def __init__(self, student, baseurl):
+		reload(sys)
+		sys.setdefaultencoding('utf-8')
+		self.student = student
+		self.baseurl = baseurl
+		self.session  = requests.session()
+		self.session.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
 
-vercode = raw_input('vercode:')
-code = driver.find_element_by_name('TextBox3')
-code.clear()
-code.send_keys(vercode)
+	def Login(self):
+		url = self.baseurl+'/default2.aspx'
+		res = self.session.get(url)
+		cont = res.content
+		selector = etree.HTML(cont)
+		__VIEWSTATE = selector.xpath('//*[@id="form1"]/input/@value')[0]
+		imgurl = self.baseurl + '/CheckCode.aspx'
+		imgres = self.session.get(imgurl, stream=True)
+		img = imgres.content
+		with open('code.jpg', 'wb') as f:
+			f.write(img)
+		jpg = Image.open('{}/code.jpg'.format(os.getcwd()))
+		jpg.show()
+		jpg.close
+		code = raw_input('Input the secret code: ')
 
-account.send_keys(Keys.RETURN)
-time.sleep(5)
+		RadioButtonList1 = u"学生".encode('gb2312', 'replace')
+		data = {
+			"__VIEWSTATE": __VIEWSTATE,
+			"txtUserName": self.student.user,
+			"TextBox1": self.student.pswd,
+			"TextBox2": self.student.pswd,
+			"txtSecretCode": code,
+   		    "RadioButtonList1": RadioButtonList1,
+		    "Button1": "", 
+ 		    "lbLanguage": ""             
+		}
+		loginres = self.session.post(url, data=data)
+		logcont = loginres.text
+		pattern = re.compile('<form name="Form1".*?action=(.*?) id="Form1">', re.S)
+		res = re.findall(pattern, logcont)
+		if res[0][17:29] == self.student.user:
+			print 'Login succeed!'
 
-source = driver.page_source
-pattern = re.compile('<span id="xhxm">(.*?)</span>', re.S)
-name = re.findall(pattern, source)
-xm = name[0][:3]
-print xm
+		pattern = re.compile('<span id="xhxm">(.*?)</span>')
+		xhxm = re.findall(pattern, logcont)
+		name = xhxm[0][:3]
+		return name
+		
 
-now_url = driver.current_url
-print now_url
-#print source
+	def GetClass(self):
+		name = self.Login()
+		urlname = urllib.quote_plus(str(name.encode('gb2312')))
+		self.session.headers['Referer'] = self.baseurl + '/xs_main.aspx?xh=' + self.student.user
+		kburl = self.baseurl + '/xskbcx.aspx?xh='+self.student.user+'&xm='+urlname+'&gnmkdm=N121603'
+		kbresponse = self.session.get(kburl)
+		kbcont = kbresponse.text
+		
+		f = open(os.getcwd()+'/ZFKB.txt', 'w')
+		pattern = re.compile('<td.*?align="Center".*?>(.*?)</td>', re.S)
+		contents = re.findall(pattern, kbcont)
+		tool = Tool()
+		cnt = 1
+		for i in contents:
+			if u'星期' in i:
+				continue
+			elif u'第' in i:
+				con = tool.replace(i)
+				f.write('\t'+str(cnt)+':'+con.encode('utf-8')+'\n')
+ 				cnt += 1
+			else:
+				continue
+		#关闭文件
+		f.close()
 
-urlxm = urllib.quote_plus(str(xm.encode('gb2312')))
-kburl = 'http://115.236.84.162/xskbcx.aspx?xh='+usr_name+'&xm='+urlxm+'&gnmkdm=N121603'
-headers = {
-	'Referer':'http://115.236.84.162/xs_main.aspx?xh='+usr_name,
-	'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
-}
-driver.get(kburl)
-driver.find_element_by_link_text('here').click()
-print driver.page_source
-driver.close()
+		print 'Load class succeed!'
 
-
-
-
-
+if __name__ == "__main__":
+	url = 'http://.........'  #教务网网站
+	user = '............'		#学号
+	pswd = '..........'			#密码
+	who = WHO(user, pswd)
+	zafu = ZAFU(who, url)
+	zafu.GetClass()
