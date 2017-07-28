61dc9fd2c017b4e60ce12ad1158b68db295a87e2
ZYSzys <1220736035@qq.com>
2017-07-28 16:25:58 +0800

    add dy2018.py

---
+++ Dy2018/dy2018.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env Python
+#-*- coding: utf-8 -*-
+
+__author__ = 'ZYSzys'
+
+import requests
+from bs4 import BeautifulSoup
+import re
+import os
+
+url = raw_input("输入网址: ") 
+res = requests.get(url)
+cont = res.content.decode('gb2312')
+
+pattern = re.compile('<tbody>(.*?)</tbody>', re.S)
+items = re.findall(pattern, cont)
+f = open(os.getcwd()+'/MoviesRes.txt', 'w')
+for i in items:
+	soup = BeautifulSoup(i)
+	ftp = soup.find_all("a")
+	res = ftp[0].get('href')
+	f.write(res.encode('utf-8')+'\n')
+f.close()
+
+print "Completed!"
